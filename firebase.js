8bbada8ef912d78dc9ded4ff69eaa18e58d36037
Saroj Rai <raisaroj360@gmail.com>
2022-03-05 20:14:10 +0545

    Updated for Seasonal Puzzles

--- firebase.js
+++ firebase.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-analytics.js";
-import { getDatabase, ref, set, get, push } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-database.js";
+import { getDatabase, ref, set, get, push, query, orderByChild } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-database.js";
 import { firebaseConfig } from "./credential.js";
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getDatabase();
 
-function uploadPuzzle(puzzle, difficultyLevel) {
-  let puzzlesRef = ref(db, "Puzzles");
+function uploadPuzzle(puzzle, season, difficultyLevel) {
+  let puzzlesRef = ref(db, "Puzzles/" + season);
   push(puzzlesRef).then(newRef => {
     push(puzzlesRef, {
       id: newRef.key,
@@ -20,6 +20,22 @@ function uploadPuzzle(puzzle, difficultyLevel) {
   });
 }
 
+async function getPuzzles(season){
+  let puzzlesRef = ref(db, "Puzzles/" + season);
+  // let puzzlesQuery = query(puzzlesRef, orderByChild("points"));
+
+  let puzzles = [];
+  let snapshot = await get(puzzlesRef);
+  if(snapshot.exists()) {
+    let puzzlesObj = snapshot.val();
+    for(const puzzleId in puzzlesObj){
+      puzzles.push(puzzlesObj[puzzleId]);
+    }
+  }
+
+  return puzzles;
+}
+
 function puzzleToString(puzzle){
   let res = "";
   for(let i = 0; i < 9; i++)
@@ -29,5 +45,5 @@ function puzzleToString(puzzle){
 }
 
 
-export { uploadPuzzle };
+export { uploadPuzzle, getPuzzles };
 
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import { generateSudoku, printPuzzle } from './SudokuLevelGenerator.test.js';
-import { uploadPuzzle } from './firebase.js';
+import { uploadPuzzle, getPuzzles } from './firebase.js';
 
 let generateButton = document.getElementById("generateButton");
 let difficultyLevel = document.getElementById("difficultyLevel");
 let uploadButton = document.getElementById("upload");
+let showButton = document.getElementById("show");
 let puzzle = null;
+
+let d = new Date();
+let season = `${d.getFullYear()}-${d.toLocaleString('default', { month: 'long'})}-${Math.floor(d.getDay()/4)}`;
 
 generateButton.addEventListener("click", (e) => {
   e.preventDefault();
@@ -12,11 +16,49 @@ generateButton.addEventListener("click", (e) => {
   printPuzzle(puzzle);
 });
 
+
 uploadButton.addEventListener("click", (e) => {
   e.preventDefault();
   if(puzzle != null){
-    uploadPuzzle(puzzle, difficultyLevel.value);
+    uploadPuzzle(puzzle, season, difficultyLevel.value);
   } else {
     output.innerHTML = "Generate first!";
   }
 });
+
+
+showButton.addEventListener("click", async (e) => {
+  e.preventDefault();
+  let puzzles = await getPuzzles(season);
+  showPuzzles(puzzles);
+});
+
+
+function showPuzzles(puzzles){
+  totalPuzzles.innerHTML = "Total: " + puzzles.length;
+  allPuzzles.innerHTML = "";
+  for(let i = 0; i < puzzles.length; i++){
+    let puzzle = puzzles[i];
+    let puzzleElement = document.createElement("div");
+    let puzzleInfo = document.createElement("p");
+    puzzleInfo.innerHTML = `<b> ${puzzle.id} </b> <br/> points: ${puzzle.points}`;
+    puzzleElement.appendChild(puzzleInfo);
+    puzzleElement.appendChild(createPuzzleElement(puzzle.puzzle));
+    allPuzzles.appendChild(puzzleElement);      
+  }
+}
+
+
+function createPuzzleElement(puzzle){
+  let container = document.createElement("p");
+  let stringOutput = "";
+  let puzzleIndex = 0;
+  for(var i = 0; i < 9; i ++){
+    for(var j = 0; j < 9; j++){
+      stringOutput += puzzle[puzzleIndex++] + (j == 8 ? "" : "--");
+    }
+    stringOutput += "\n <br>";
+  }
+  container.innerHTML += stringOutput;
+  return container;
+}
